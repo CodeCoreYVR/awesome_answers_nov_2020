b0e6d8286f6513d4210f7e208f36a395fd364fb1
Naveed Raza <naveedraza@gmail.com>
2021-01-21 11:41:27 -0800

    testing for edit authorisation

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -1,18 +1,12 @@
 class JobPostsController < ApplicationController
     before_action :authenticate_user!, except:[:index,:show]
-    before_action :find_job_post,only:[:show,:update, :destroy]
+    before_action :find_job_post,only:[:show,:update, :destroy, :edit]
     def new
         @job_post=JobPost.new
     end
     def create
-        @job_post=JobPost.new params.require(:job_post)
-        .permit(
-            :title,
-            :description,
-            :min_salary,
-            :max_salary,
-            :company_name,
-            :location)
+        @job_post=JobPost.new job_post_params
+       
         @job_post.user=current_user
         if @job_post.save
             redirect_to job_post_path(@job_post)
@@ -27,18 +21,19 @@ class JobPostsController < ApplicationController
     def index
         @job_posts=JobPost.all.order(created_at: :desc)
     end
+    # From Ken
     def edit
+        # @job_post=JobPost.find params[:id]
+        if can?(:edit, @job_post)
+            render :edit
+        else
+            redirect_to job_post_path(@job_post)
+        end
     end
+    
     def update
         
-        if @job_post.update params.require(:job_post)
-        .permit(
-            :title,
-            :description,
-            :min_salary,
-            :max_salary,
-            :company_name,
-            :location)
+        if @job_post.update job_post_params
             redirect_to @job_post
         end
     end
@@ -55,5 +50,15 @@ class JobPostsController < ApplicationController
     private
     def find_job_post
         @job_post=JobPost.find params[:id]
+    end
+    def job_post_params
+        params.require(:job_post)
+        .permit(
+            :title,
+            :description,
+            :min_salary,
+            :max_salary,
+            :company_name,
+            :location)
     end
 end
