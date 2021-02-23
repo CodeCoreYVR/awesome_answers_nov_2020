ec61072bb9ae9c8ebd772c48cc7188501034bf24
Naveed Raza <naveedraza@gmail.com>
2021-02-23 09:58:31 -0800

    sending email using letter opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,17 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true # 👈🏻 For Maielrs
+  # The above setting should be set to 'true ' otherwise all your mails will fail silently,
+  
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options ={
+    host: "localhost:3000"
+  }
+
+
+  
 
   config.action_mailer.perform_caching = false
 
